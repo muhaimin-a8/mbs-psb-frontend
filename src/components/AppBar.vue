c34c01e8d6e87e4c4cd1d2f20fed094056bf8204
Muhaimin Abdillah <muhaiminabdillah85@gmail.com>
2022-10-24 03:39:50 +0700

    add appbar

---
+++ src/components/AppBar.vue
@@ -0,0 +1,58 @@
+<template>
+<div id="appbar">
+  <a href="#"><img src="@/assets/mbs-logo.png" alt="logo mbs purworejo"></a>
+  <nav>
+    <ul>
+      <li><a href="#">Home</a></li>
+      <li><a href="#">Timeline</a></li>
+      <li><a href="#">Contact</a></li>
+      <li class="btn"><a href="#">Login</a></li>
+    </ul>
+  </nav>
+</div>
+</template>
+
+<script>
+export default {
+  name: "AppBar"
+}
+</script>
+
+<style scoped>
+#appbar {
+  display: flex;
+  background-color: var(--vt-c-white);
+  padding: 18px;
+  justify-content: space-between;
+  align-items: center;
+}
+
+img {
+  height: 50px;
+}
+
+ul li {
+  display: inline;
+  margin-right: 18px;
+}
+
+ul li a {
+  color: var(--primary-color);
+}
+ul li:not(.btn) a:hover::after{
+  content: '';
+  width: 18px;
+  height: 4px;
+  border-radius: 5px;
+  position: absolute;
+  transform: translate(calc(-50% - 18px), -12px);
+  background-color: var(--primary-color);
+}
+
+ul .btn a {
+  background-color: var(--primary-color);
+  padding: 8px 16px;
+  border-radius: 25px;
+  color: var(--vt-c-text-dark-1);
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,10 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+// import TheWelcome from '../components/TheWelcome.vue'
+import AppBar from "../components/AppBar.vue";
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <AppBar />
   </main>
 </template>
